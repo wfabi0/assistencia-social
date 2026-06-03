48eba1291734e29b70d6bf2b4b44e47b16a77e7f
Felipe-m-s <fs33143martins@gmail.com>
2026-06-03 20:16:09 -0300

    criação do modelo endereço

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Endereco(models.Model):
+    logradouro = models.CharField("Logradouro", max_length=255)
+    numero = models.CharField("Número", max_length=10)
+    bairro = models.CharField("Bairro", max_length=255)
+    cidade = models.CharField("Cidade", max_length=255)
+    estado = models.CharField("Estado", max_length=255)
+    cep = models.CharField("CEP", max_length=10)
+    
+    class Meta:
+        verbose_name = "Endereço"
+        verbose_name_plural = "Endereços"
+        db_table = "endereco"
+        
+    def __str__(self):
+        return f"{self.logradouro}, {self.numero} - {self.bairro}, {self.cidade} - {self.estado}, CEP: {self.cep}"
