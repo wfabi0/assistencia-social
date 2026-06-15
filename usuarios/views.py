1b36083e80e0c271a8c55e1ff0e712e1ef781236
Teyllor-Rodrigues <Tayllonjunior639@gmail.com>
2026-06-15 11:03:48 -0300

    templates base e telas de alunos

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def aluno_list(request):
+    return render(request, 'usuarios/aluno_list.html')
+
+def aluno_form(request):
+    return render(request, 'usuarios/aluno_form.html')
+
+def aluno_detail(request):
+    return render(request, 'usuarios/aluno_detail.html')
+
+def aluno_update(request, pk=None):
+    return render(request, 'usuarios/aluno_form.html')
+
+def aluno_historico(request, pk=None):
+    return render(request, 'usuarios/aluno_detail.html')
