9afb37165a8341b4bffd4b5697accf5e4b466377
Felipe-m-s <fs33143martins@gmail.com>
2026-06-17 21:34:40 -0300

    adicição da view List e templates para teste de cadastro e lista de atendimentos

--- atendimentos/urls.py
+++ atendimentos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("atendimentos/", views.AtendimentoListView.as_view(), name="lista_atendimentos"),
     path("atendimentos/novo/", views.AtendimentoCreateView.as_view(), name="novo_atendimento"),
 ]
--- atendimentos/views.py
+++ atendimentos/views.py
@@ -4,6 +4,25 @@ from .models import Atendimento
 from .forms import AtendimentoForm
 
 # Create your views here.
+class AtendimentoListView(ListView):
+    model = Atendimento
+    template_name = "atendimentos/list.html"
+    context_object_name = "atendimentos"
+    # paginate_by = 10
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get("q")
+        if search_query:
+            queryset = queryset.filter(
+                aluno__nome__icontains=search_query
+            ) | queryset.filter(
+                servidor__nome__icontains=search_query
+            ) | queryset.filter(
+                usuario_externo__nome__icontains=search_query
+            )
+        return queryset.order_by("-data_atendimento")
+
 class AtendimentoCreateView(CreateView):
     model = Atendimento
     form_class = AtendimentoForm
