8eb5e2b476a5a44bacf1a25b136aaf56520046e5
Felipe-m-s <fs33143martins@gmail.com>
2026-06-17 22:05:01 -0300

    adicição de paginação e campo de busca à lista de atendimentos

--- atendimentos/views.py
+++ atendimentos/views.py
@@ -8,7 +8,14 @@ class AtendimentoListView(ListView):
     model = Atendimento
     template_name = "atendimentos/list.html"
     context_object_name = "atendimentos"
-    # paginate_by = 10
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("por_pagina", 10)  # padrão 10
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["por_pagina"] = self.request.GET.get("por_pagina", self.get_paginate_by(None))
+        return context
     
     def get_queryset(self):
         queryset = super().get_queryset()
