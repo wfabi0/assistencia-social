e18900a92f583c0218e932487a0508963f68bda6
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2026-06-25 20:01:33 -0300

    parei na autenticaçao

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('usuarios.urls')),
     path('', include('atendimentos.urls')),
 ]
