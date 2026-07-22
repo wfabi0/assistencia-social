afc95d087af60e1f422ccba9b28446dad62b5b98
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2026-07-22 18:27:37 -0300

    config de models no admin django

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Usuario, Aluno, Servidor, UsuarioExterno, Endereco, Responsavel
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(Aluno)
+admin.site.register(Servidor)
+admin.site.register(UsuarioExterno)
+admin.site.register(Endereco)
+admin.site.register(Responsavel)
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Dados Profissionais', {
+            'fields': ('cress',)
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Dados Profissionais', {
+            'fields': ('cress',)
+        }),
+    )
